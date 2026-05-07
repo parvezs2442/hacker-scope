5439c926a8866a85ca70cb506127ba823db6e00d
Parvez Saifi <parvezs2442@gmail.com>
2026-05-07 21:34:12 +0530

    Setup MongoDB connection and create database models

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
+const connectDB = require("./config/db");
+
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
